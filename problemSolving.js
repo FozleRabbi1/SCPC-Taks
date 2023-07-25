ebd3b9c413b2458e4574f61cf0984be521cd2729
Fozle Rabbi <fozlerabbi9790@gmail.com>
2023-07-25 14:22:07 +0600

    done

--- problemSolving.js
+++ problemSolving.js
@@ -1,7 +1,6 @@
 
 //Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
 // Example Input: "hello world" Example Output: "dlrow olleh"
-
 const reversedVersion = (string) => {
     const lowerString = string.toLowerCase();
     const reversedString = [];
